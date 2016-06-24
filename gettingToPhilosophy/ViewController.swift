c457812e397f3a586ee826add4e09814c98aa564
Ikkyu0 <pbohnert@gmail.com>
2016-06-24 11:27:35 -0700

    Initial Commit

---
+++ gettingToPhilosophy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gettingToPhilosophy
+//
+//  Created by Peter Bohnert on 6/24/16.
+//  Copyright © 2016 bluelotuslabs.co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
